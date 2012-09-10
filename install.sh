a6b03424891f0a1679231e4a2b32fc9a0fdb007a
Julien Rouhaud <julien.rouhaud@free.fr>
2012-09-10 20:24:14 +0200

    Relative directory modification

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-~/travail/pgxc1.0.1/install_xc.sh -d ~/travail/pgxctest -b ~/travail/pgxc1.0.1/bin > install.log 2>&1
+./install_xc.sh -d ~/travail/pgxctest -b ~/travail/pgxc1.0.1/bin > install.log 2>&1
