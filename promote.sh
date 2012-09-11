8ebbd45de4ac4a12b593e401dbad8215941aee2f
Julien Rouhaud <julien.rouhaud@free.fr>
2012-09-11 21:52:46 +0200

    Adding some url

--- promote.sh
+++ promote.sh
@@ -2,5 +2,5 @@
 echo "Promoting Standby GTM..."
 ~/travail/pgxc1.0.1/bin/gtm_ctl -Z gtm -D ~/travail/pgxctest/gtm2/ promote
 echo "Notifying GTM_proxy"
-~/travail/pgxc1.0.1/bin/gtm_ctl -Z gtm_proxy -D ~/travail/pgxctest/gtm_proxy/ -o "-s localhost -t 6669" reconnect 
+~/travail/pgxc1.0.1/bin/gtm_ctl -Z gtm_proxy -D ~/travail/pgxctest/gtm_proxy/ -o "-s 127.0.0.1 -t 6669" reconnect 
 
